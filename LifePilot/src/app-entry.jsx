cd3024ddb83dc05e08de9a542837e7466ff6e476
SosaBit <brandecho2k25@gmail.com>
2026-08-22 01:34:20 +0200

    Add isolated canonical app entrypoint

---
+++ LifePilot/src/app-entry.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import LifePilotV2 from './lifepilot-v2.jsx';
+import './lifepilot-v2.css';
+
+createRoot(document.getElementById('root')).render(<LifePilotV2 />);
