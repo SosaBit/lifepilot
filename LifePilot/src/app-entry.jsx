ca809cbbe6616c5b404ee6d8d39900cb3be75e41
SosaBit <brandecho2k25@gmail.com>
2026-08-22 01:37:25 +0200

    Load billing controls with product shell

--- LifePilot/src/app-entry.jsx
+++ LifePilot/src/app-entry.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import LifePilotV2 from './lifepilot-v2.jsx';
 import './lifepilot-v2.css';
+import './billing-enhancer.js';
 
 createRoot(document.getElementById('root')).render(<LifePilotV2 />);
