8b28d1a4af1e864e3711cc6337cc3e490c1cd956
SosaBit <brandecho2k25@gmail.com>
2026-08-22 01:37:38 +0200

    Load admin console enhancer

--- LifePilot/src/app-entry.jsx
+++ LifePilot/src/app-entry.jsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import LifePilotV2 from './lifepilot-v2.jsx';
 import './lifepilot-v2.css';
 import './billing-enhancer.js';
+import './admin-enhancer.js';
 
 createRoot(document.getElementById('root')).render(<LifePilotV2 />);
