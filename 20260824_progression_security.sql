227ea2276bdf26ace6ec0baaec02b578c6981624
SosaBit <brandecho2k25@gmail.com>
2026-08-24 20:52:32 +0200

    chore: remove root duplicate SQL artifacts

--- 20260824_progression_security.sql
+++
@@ -1,4 +0,0 @@
-revoke execute on function public.complete_life_session(uuid) from anon;
-revoke execute on function public.record_quiz_result(text,uuid,uuid,integer,integer,text[]) from anon;
-revoke execute on function public.evaluate_progression() from anon,authenticated;
-revoke execute on function public.trigger_evaluate_progression() from anon,authenticated;
